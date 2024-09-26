66d6a12978e4bb0cc8e0c1c9798888d3699c18a7
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2024-09-26 17:06:23 +0700

    Implement proxy pattern and decorator pattern for caching basket repository, register decorator pattern using Scrutor library

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -19,6 +19,13 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
