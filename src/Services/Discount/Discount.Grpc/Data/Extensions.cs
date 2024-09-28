0b57cdc26b8e6f7c94e50cfbe92befd50a819f93
zZMinatoZz <heaven1xx3@gmail.com>
2024-09-28 17:35:04 +0700

    Create discount service (#3)
    * initialize Discount service using gRPC, EF core, Sqlite
    
    * CRUD for discount using gRPC, setup to dockerize discount service

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+public static class Extensions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
