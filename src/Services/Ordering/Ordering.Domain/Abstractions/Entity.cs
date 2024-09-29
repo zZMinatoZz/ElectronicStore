d78940286cb3aa279583d82bfd849acdaaab575a
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2024-09-29 18:33:39 +0700

    develop DDD abtractions (Entity, Aggregate, DomainEvent)

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions;
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
