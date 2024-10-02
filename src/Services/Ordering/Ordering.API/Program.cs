c60fd391986619969ca807636333d9c2914ab9e8
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2024-10-02 10:34:21 +0700

    seeding data

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,4 +15,10 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseApiServices();
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
+
 app.Run();
