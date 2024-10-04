c6e8b60eb6419f12de7a18f44065caea3e815c8d
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2024-10-04 11:32:49 +0700

    handling get orders query using cqrs pattern, implement pagination

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders.Include(o => o.OrderItems)
+                                .AsNoTracking()
+                                .Where(o => o.OrderName.Value.Contains(query.Name))
+                                .OrderBy(o => o.OrderName)
+                                .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
